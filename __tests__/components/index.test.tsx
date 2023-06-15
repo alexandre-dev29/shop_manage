6a4c0741c62f59f84f4edf84f9af4804857f4a1e
alexandre-dev29 <axel.business29@gmail.com>
2023-06-15 14:24:57 +0200

    resolve test problems

--- __tests__/components/index.test.tsx
+++ __tests__/components/index.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from "@testing-library/react"
 
 describe("should run propertly", function () {
   it("should has value", function () {
-    render(<h1 data-testid="headingMain">This is my shop</h1>)
+    render(<h1 data-testid="headingMain">This is my shop manage</h1>)
     const element = screen.getByTestId("headingMain")
     expect(element).toHaveTextContent("This is my shop manage")
   })
